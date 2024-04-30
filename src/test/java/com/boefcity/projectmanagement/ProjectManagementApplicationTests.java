2ca64f90004d2bda0dfcba7fa51242ebca2503e7
wzNaji <ludi2300@gmail.com>
2024-04-30 07:44:28 +0200

    CONNECTED TO DATABASE + GITHUB SECRETS FOR SPRING.DATASOURCE

--- src/test/java/com/boefcity/projectmanagement/ProjectManagementApplicationTests.java
+++ src/test/java/com/boefcity/projectmanagement/ProjectManagementApplicationTests.java
@@ -2,8 +2,10 @@ package com.boefcity.projectmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProjectManagementApplicationTests {
 
     @Test
