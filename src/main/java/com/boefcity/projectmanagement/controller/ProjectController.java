52bb4b40fc0f48fec17437a515a990e99779bd50
milasKEA <miso0002@stud.kea.dk>
2024-05-03 19:05:26 +0200

    added ProjectController (v0.1)

---
+++ src/main/java/com/boefcity/projectmanagement/controller/ProjectController.java
@@ -0,0 +1,25 @@
+package com.boefcity.projectmanagement.controller;
+
+import com.boefcity.projectmanagement.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @Autowired
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/display")
+    public String projectsDisplay(){
+        return "projects";
+    }
+
+}
