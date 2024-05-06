0855739331d97d774842f05d44cb828208755680
wzNaji <ludi2300@gmail.com>
2024-05-06 17:45:47 +0200

    Fixed password encryption duplication. Added functionality to loginPage + added errorPage

--- src/main/java/com/boefcity/projectmanagement/controller/UserController.java
+++ src/main/java/com/boefcity/projectmanagement/controller/UserController.java
@@ -3,13 +3,11 @@ package com.boefcity.projectmanagement.controller;
 import com.boefcity.projectmanagement.model.Role;
 import com.boefcity.projectmanagement.model.User;
 import com.boefcity.projectmanagement.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
@@ -32,10 +30,28 @@ public class UserController {
     }
     @GetMapping("/registerDisplay")
     public String registerDisplay(Model model) {
-
         model.addAttribute("user", new User());
         return "registerPage";
     }
+    @PostMapping("/registerUser")
+    public String registerUser(@ModelAttribute User user,
+                               RedirectAttributes redirectAttributes) {
+        try {
+            Optional<User> optionalExistingUser = userService.findUserByUsername(user.getUsername());
+            if (optionalExistingUser.isPresent()) {
+                redirectAttributes.addFlashAttribute("message", "Username already exists.");
+                return "redirect:/users/registerDisplay";
+            }
+            userService.createUser(user);
+            redirectAttributes.addFlashAttribute("message", "User registered successfully!");
+            return "redirect:/users/loginDisplay";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "An error occurred.");
+            return "redirect:/users/registerDisplay";
+        }
+    }
+
+    /*
     @PostMapping("/registerUser")
     public String registerUser(@ModelAttribute User user,
                                RedirectAttributes redirectAttributes) {
@@ -55,5 +71,28 @@ public class UserController {
             return "redirect:/users/registerDisplay";
         }
     }
+
+     */
+    @PostMapping("/loginUser")
+    public String loginUser(@RequestParam String username,
+                            @RequestParam String password,
+                            HttpSession session,
+                            RedirectAttributes redirectAttributes) {
+
+        boolean isValidUser = userService.checkLogin(username, password);
+        if (isValidUser) {
+            Optional<User> optionalUser = userService.findUserByUsername(username);
+            if (optionalUser.isPresent()) {
+                User user = optionalUser.get();
+                session.setAttribute("userId", user.getUserId());
+                return "redirect:/projects/display";
+            }
+        } else {
+            redirectAttributes.addFlashAttribute("message", "Incorrect username or password. Try again");
+            return "redirect:/users/loginDisplay";
+        }
+        return "redirect:/users/loginDisplay";
+    }
+
 
 }
--- src/main/java/com/boefcity/projectmanagement/service/implementation/UserServiceImpl.java
+++ src/main/java/com/boefcity/projectmanagement/service/implementation/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.boefcity.projectmanagement.service.implementation;
 
+import com.boefcity.projectmanagement.model.Role;
 import com.boefcity.projectmanagement.model.User;
 import com.boefcity.projectmanagement.repository.UserRepository;
 import com.boefcity.projectmanagement.service.UserService;
@@ -30,6 +31,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(User user) {
+        user.setUserRole(Role.WORKER);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
