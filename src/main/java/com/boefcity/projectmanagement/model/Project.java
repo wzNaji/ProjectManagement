e39a68d37f7bcb265ab2d024e7b7323278146dbd
wzNaji <ludi2300@gmail.com>
2024-05-14 18:32:28 +0200

    Added budget, cost, estimated hours, actual hours, fields for projects.

--- src/main/java/com/boefcity/projectmanagement/model/Project.java
+++ src/main/java/com/boefcity/projectmanagement/model/Project.java
@@ -38,7 +38,6 @@ public class Project {
             joinColumns = @JoinColumn(name = "project_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id")
     )
-
     private List<User> users = new ArrayList<>();
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
@@ -47,8 +46,19 @@ public class Project {
             joinColumns = @JoinColumn(name = "project_id"),
             inverseJoinColumns = @JoinColumn(name = "task_id")
     )
-
     private List<Task> tasks = new ArrayList<>();
+
+    @Column(name = "project_budget", nullable = true)
+    private Double projectBudget;
+
+    @Column(name = "project_cost", nullable = true)
+    private Double projectCost;
+
+    @Column(name = "project_esitmated_hours", nullable = true)
+    private Double projectEstimatedHours;
+
+    @Column(name = "project_actual_hours", nullable = true)
+    private Double projectActualdHours;
 
     public void addUser(User user) {
         users.add(user);
