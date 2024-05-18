441f7c102fa0a4b9e2b5efb01a0dfcac8bfc4a02
wzNaji <ludi2300@gmail.com>
2024-05-18 05:45:51 +0200

    Added: Edit task functionality and minor changes

--- src/main/java/com/boefcity/projectmanagement/controller/TaskController.java
+++ src/main/java/com/boefcity/projectmanagement/controller/TaskController.java
@@ -82,4 +82,93 @@ public class TaskController {
 
     }
 
+    @GetMapping("/editFormDisplay/{taskId}")
+    public String editTaskFormDisplay(@PathVariable Long taskId,
+                                      @RequestParam Long projectId,
+                                      @RequestParam Long subprojectId,
+                                      Model model,
+                                      HttpSession session,
+                                      RedirectAttributes redirectAttributes) {
+
+        if (AppUtility.isNotAuthenticated(session, redirectAttributes)) {
+            return "redirect:/users/loginDisplay";
+        }
+        Project project = projectService.findProjectById(projectId);
+        Subproject subproject = subprojectService.findBySubprojectId(subprojectId);
+        Task taskToEdit = taskService.findByTaskId(taskId);
+        if (taskToEdit == null) {
+            redirectAttributes.addFlashAttribute("message", "Task til redigering blev ikke fundet");
+            return "redirect:/subprojects/overviewDisplay?subprojectId=" + subprojectId + "&projectId=" + projectId;
+        }
+
+        model.addAttribute("priorityLevel", PriorityLevel.values());
+        model.addAttribute("status", Status.values());
+        model.addAttribute("task", taskToEdit);
+        model.addAttribute("project", project);
+        model.addAttribute("subproject", subproject);
+        model.addAttribute("task", taskToEdit);
+        return "project/subproject/task/taskEditForm";
+    }
+
+    @PostMapping("/editForm/{taskId}")
+    public String editTask(@PathVariable Long taskId,
+                           @RequestParam Long subprojectId,
+                           @RequestParam Long projectId,
+                           @ModelAttribute("task") Task taskDetails,
+                           HttpSession session,
+                           RedirectAttributes redirectAttributes) {
+
+        if (AppUtility.isNotAuthenticated(session, redirectAttributes)) {
+            return "redirect:/users/loginDisplay";
+        }
+
+        Task existingTask = taskService.findByTaskId(taskId);
+        if (existingTask == null) {
+            redirectAttributes.addFlashAttribute("message", "Task til redigering blev ikke fundet");
+            return "redirect:/subprojects/overviewDisplay?subprojectId=" + subprojectId + "&projectId=" + projectId;
+        }
+
+        try {
+            taskService.editTask(taskId, taskDetails);
+            redirectAttributes.addFlashAttribute("message", "Task blev opdateret");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "Noget gik galt. Prøv venligst igen.");
+        }
+
+        return "redirect:/subprojects/overviewDisplay?subprojectId=" + subprojectId + "&projectId=" + projectId;
+    }
+
+    @PostMapping("/delete")
+    public String deleteTask(@RequestParam("taskId") Long taskId,
+                             @RequestParam("subprojectId") Long subprojectId,
+                             @RequestParam("projectId") Long projectId,
+                             HttpSession session,
+                             RedirectAttributes redirectAttributes) {
+
+        if (AppUtility.isNotAuthenticated(session, redirectAttributes)) {
+            return "redirect:/users/loginDisplay";
+        }
+
+        Long userId = (Long) session.getAttribute("userId");
+        User user = userService.findUserById(userId);
+        if (!AppUtility.isAdminOrManager(user)) {
+            redirectAttributes.addFlashAttribute("message", "Kun ADMIN og MANAGER brugere har tilladelse til sletning");
+            return "redirect:/subprojects/overviewDisplay?subprojectId=" + subprojectId + "&projectId=" + projectId;
+        }
+
+        try {
+            Task taskToDelete = taskService.findByTaskId(taskId);
+            if (taskToDelete == null) {
+                redirectAttributes.addFlashAttribute("message", "Task blev ikke fundet");
+            } else {
+                taskService.deleteTask(taskId, subprojectId);
+                redirectAttributes.addFlashAttribute("message", "Task blev slettet");
+            }
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", "Fejl under sletning af task");
+        }
+
+        return "redirect:/subprojects/overviewDisplay?subprojectId=" + subprojectId + "&projectId=" + projectId;
+    }
+
 }
